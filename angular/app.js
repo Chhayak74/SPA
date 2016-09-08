fa0eee1f60e7cb9638c57e6b467c0f66f3828076
Chhaya Kashyap <chhayak74@gmail.com>
2016-09-09 04:38:29 +0530

    Modified url

--- angular/app.js
+++ angular/app.js
@@ -17,7 +17,7 @@ myApp.controller('mainController',['$http',function($http) {
   this.blogs = [];
   console.log(this.blogs);
 
-  this.baseUrl = 'http://ec2-52-77-247-196.ap-southeast-1.compute.amazonaws.com:3000/api/blogs';
+  this.baseUrl = '//ec2-52-77-247-196.ap-southeast-1.compute.amazonaws.com:3000/api/blogs';
 
 
 
@@ -96,7 +96,7 @@ myApp.controller('singleBlogController',['$http','$routeParams',function($http,$
   console.log(this.blogId);
 
 
-  this.baseUrl = 'http://ec2-52-77-247-196.ap-southeast-1.compute.amazonaws.com:3000/api/blogs';
+  this.baseUrl = '//ec2-52-77-247-196.ap-southeast-1.compute.amazonaws.com:3000/api/blogs';
 
   this.delete =function(){
 
@@ -157,7 +157,7 @@ myApp.controller('blogCreateController',['$http',function($http) {
   this.pageSubHeading = 'please fill all the data'
  
 
-  this.baseUrl = 'http://ec2-52-77-247-196.ap-southeast-1.compute.amazonaws.com:3000/api/blogs';
+  this.baseUrl = '//ec2-52-77-247-196.ap-southeast-1.compute.amazonaws.com:3000/api/blogs';
 
   this.createPost = function(){
 
@@ -225,7 +225,7 @@ myApp.controller('editableController',['$http','$routeParams',function($http,$ro
   console.log(this.blogId);
 
 
-  this.baseUrl = 'http://ec2-52-77-247-196.ap-southeast-1.compute.amazonaws.com:3000/api/blogs';
+  this.baseUrl = '//ec2-52-77-247-196.ap-southeast-1.compute.amazonaws.com:3000/api/blogs';
 
   this.loadSingeBlog = function(){
 
